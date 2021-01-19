bedde5e32df95441591912421298658466d5c9f4
AndyNgoIT <dongngo.2000@gmail.com>
2021-01-19 22:36:10 +0700

    andyngo

--- android/app/src/main/java/com/dobeemovie/MainApplication.java
+++ android/app/src/main/java/com/dobeemovie/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -46,6 +48,15 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          return Arrays.<ReactPackage>asList(
+           new MainReactPackage(),
+           new LinearGradientPackage(), // <---- and This! 
+           new VectorIconsPackage()
+          );
+        }
 
   /**
    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
