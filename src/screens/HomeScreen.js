4522f201570ed79254a9a4627abc5f40e6fdf620
Minh <sybertooth4945@gmail.com>
2021-01-30 15:37:46 +0700

    feat: fast image

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 import { HeaderComponent} from '../components';
+import FastImage from 'react-native-fast-image';
 
 export default class HomeScreen extends Component {
     render() {
@@ -11,7 +12,19 @@ export default class HomeScreen extends Component {
                 onPress = {() => this.props.navigation.navigate('DetailScreen')}>
                     <Text>Spirited Away</Text>
                 </TouchableOpacity>
+                <FastImage
+                    source={require('../assets/images/avatar.webp')}
+                    style={styles.viewAvatar}
+                />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    viewAvatar: {
+        width: 300,
+        height: 300,
+        marginTop: 24
+    }
+})
